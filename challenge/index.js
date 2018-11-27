24e4936a586d67d8f7bffa5f7369b67e3d9c30cc
jeffrey <jeffreyneilflynn@gmail.com>
2018-11-27 11:49:45 -0500

    format data

--- challenge/index.js
+++ challenge/index.js
@@ -2,6 +2,10 @@ const tf = require('@tensorflow/tfjs');
 const iris = require('./iris.json');
 const irisTesting = require('./testingIris.json');
 
+const trainingData = tf.tensor2d(iris.map(item=> [
+  item.sepal_length, item.sepal_width, item.petal_length, item.petal_width
+]),[130,4]);
+
 // convert data
 
 
