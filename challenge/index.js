3feaa1c70820d80dcd84ea7a8e565c4d9bcf1b95
jeffrey <jeffreyneilflynn@gmail.com>
2018-11-29 12:54:33 -0500

    output data mapping

--- challenge/index.js
+++ challenge/index.js
@@ -10,6 +10,15 @@ const testingData = tf.tensor2d(irisTesting.map(item => [
   item.sepal_length, item.sepal_width, item.petal_length, item.petal_width
 ]), [14, 4]);
 
+/* The output will be based on the neuron activation. 
+We will write the output function such that we get an array of length three 
+every time with one of the values closer to one and the rest two closer to zero. */
+const outputData = tf.tensor2d(iris.map(item => [
+  item.species === 'setosa' ? 1 : 0,
+  item.species === 'virginica' ? 1 : 0,
+  item.species === 'versicolor' ? 1 : 0
+]), [130,3]);
+
 // convert data
 
 
