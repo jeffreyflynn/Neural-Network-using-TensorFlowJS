7308d41c44111c8a1da0fdda334a00247480a857
jeffrey <jeffreyneilflynn@gmail.com>
2018-11-29 12:51:12 -0500

    testing data mapping

--- challenge/index.js
+++ challenge/index.js
@@ -6,6 +6,10 @@ const trainingData = tf.tensor2d(iris.map(item=> [
   item.sepal_length, item.sepal_width, item.petal_length, item.petal_width
 ]),[130,4]);
 
+const testingData = tf.tensor2d(irisTesting.map(item => [
+  item.sepal_length, item.sepal_width, item.petal_length, item.petal_width
+]), [14, 4]);
+
 // convert data
 
 
